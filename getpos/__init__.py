4b620dce827933ec845222f87a195c68f49408bd
sammish <sammish.thundiyil@gmail.com>
2025-08-20 23:21:11 +0530

    update

--- getpos/__init__.py
+++ getpos/__init__.py
@@ -1,9 +1,10 @@
 
 __version__ = '0.0.1'
 
-from frappe.core.doctype.user.user import User
-
-
-from getpos.overrides.verification import custom_send_login_mail
-
-User.send_login_mail=custom_send_login_mail
+try:
+    from frappe.core.doctype.user.user import User
+    from getpos.overrides.verification import custom_send_login_mail
+    User.send_login_mail = custom_send_login_mail
+except ImportError:
+    # frappe not available during installation
+    pass
